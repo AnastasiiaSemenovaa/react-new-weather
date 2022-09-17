f6dcc1c65edae86d14d5112abc870a33c3f11d41
AnastasiiaSemenovaa <semenovaanastasiiaa@gmail.com>
2022-09-17 12:39:35 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Weather from "./Weather";
 export default function App() {
   return (
     <div>
-      <Weather defaultCity="New York" />
+      <Weather defaultCity="Kyiv" />
     </div>
   );
 }
